41d6bd2e6998d8ba5da088500660175e85dc1baa
yodofaither <yodofaither@outlook.com>
2020-06-23 19:43:06 +0800

    reactjs_code_snippets test succees

---
+++ reactjs_code_snippets_test.js
@@ -0,0 +1,14 @@
+// 输入rcc,VScode中reactjs_code_snippets插件会帮你自动补全下面代码
+import React, { Component } from 'react';
+
+class reactjs_code_snippets_test extends Component {
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default reactjs_code_snippets_test;
